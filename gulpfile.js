6cd9598266bb16117df80c3a0aa3fd6ecf1d4fed
azwier <amized@gmail.com>
2016-10-25 20:26:03 -0400

    Js file headers

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,57 @@ var watchify = require('watchify');
 var babel = require('babelify');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var fs = require('fs');
+var banner = require('gulp-banner');
+var pkg = require('./package.json');
+
+var comment = '/*\n' +
+    ' * <%= pkg.name %> <%= pkg.version %>\n' +
+    ' * <%= pkg.description %>\n' +
+    ' * <%= pkg.homepage %>\n' +
+    ' *\n' +
+    ' * Copyright 2016, <%= pkg.author %>\n' +
+    ' * Released under the <%= pkg.license %> license.\n' +
+    '*/\n\n';
 
 var DEST = 'dist/';
 
+var reBannerComment = new RegExp('^\\s*(?:\\/\\*[\\s\\S]*?\\*\\/)\\s*');
+
+function getBanner() {
+  var src = fs.readFileSync( 'masonry.js', 'utf8' );
+  var matches = src.match( reBannerComment );
+  var banner = matches[0].replace( 'Masonry', 'Masonry PACKAGED' );
+  return banner;
+}
+
+function addBanner( str ) {
+  return replace( /^/, str );
+}
+
+
 function compile(watch) {
-  var bundler = watchify(browserify('./src/Stickytable.js', { debug: true, standalone: "Stickytable" }).transform(babel, {presets: ["es2015"]}));
+  var bundler = watchify(browserify('./src/Stickytable.js', { 
+    debug: true, 
+    standalone: "Stickytable"
+  }).transform(babel, {presets: ["es2015"]}));
 
   function rebundle() {
     bundler.bundle()
       .on('error', function(err) { console.error(err); this.emit('end'); })
       .pipe(source('Stickytable.js'))
       .pipe(buffer())
+      .pipe(banner(comment, {
+            pkg: pkg
+        }))
       .pipe(gulp.dest(DEST))
-      .pipe(uglify())
+      .pipe(uglify({
+        mangle: true,
+        compress: true
+      }))
+      .pipe(banner(comment, {
+            pkg: pkg
+        }))
       .pipe(rename({ extname: '.min.js' }))
       .pipe(gulp.dest(DEST));
   }
